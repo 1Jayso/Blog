3e6ef7efbe53b822d5740c063b9dcae3c353a2d3
1Jayso <adodey1@yahoo.com>
2020-07-14 07:20:07 +0000

    Created a database model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+# Register the Post model
+admin.site.register(Post)
+
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
+from .models import Post
 
 
-posts = [
-    {   'author' : 'Joseph Sowah',
-        'title' : 'The baker and the beauty',
-        'content' : 'Its a nice story about how a baker fell in love',
-        'date_posted' : 'April 12, 2020'
-    },
+# posts = [
+#     {   'author' : 'Joseph Sowah',
+#         'title' : 'The baker and the beauty',
+#         'content' : 'Its a nice story about how a baker fell in love',
+#         'date_posted' : 'April 12, 2020'
+#     },
 
-    {
-        'author' : 'Blessing Adarkwa',
-        'title' : 'God is all powerful',
-        'content' : 'The nature and power of God',
-        'date_posted' : 'August 15, 2020'
+#     {
+#         'author' : 'Blessing Adarkwa',
+#         'title' : 'God is all powerful',
+#         'content' : 'The nature and power of God',
+#         'date_posted' : 'August 15, 2020'
         
-    }
-]
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
