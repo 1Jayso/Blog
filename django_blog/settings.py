a0ad094e37f7d8258799f37dfbee813934e23997
1Jayso <adodey1@yahoo.com>
2020-07-15 00:17:14 +0000

    Added user registration

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -28,10 +28,14 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 # Application definition
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ users/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import userRegisterForm
+
+
+def register(request):
+    
+    if request.method == 'POST':
+        form = userRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account created for {username}!")
+            return redirect('blog-home')
+    else:
+        form = userRegisterForm()
+
+
+    return render(request, 'users/register.html', {'form': form})
