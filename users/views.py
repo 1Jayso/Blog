6eed11503f1137f75322951e7a948cb6cdc08dde
1Jayso <adodey1@yahoo.com>
2020-07-23 06:03:56 +0000

    Added authentication

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import userRegisterForm
 
@@ -11,10 +11,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Account created for {username}!")
-            return redirect('blog-home')
+            messages.success(request, f" Your account has been created! You are now able to login")
+            return redirect('login')
     else:
         form = userRegisterForm()
 
 
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
