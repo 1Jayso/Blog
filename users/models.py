0d3c827fdb89cb86322795205593d14af03d8f0e
1Jayso <adodey1@yahoo.com>
2020-08-28 16:04:19 +0000

    change profile image size

--- users/models.py
+++ users/models.py
@@ -15,8 +15,8 @@ class Profile(models.Model):
 
 
         img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+        if img.height > 320 or img.width > 320:
+            output_size = (320, 320)
             img.thumbnail(output_size)
             img.save(self.image.path)
 
