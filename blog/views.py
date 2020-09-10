cf13be210efadab471c45736cd6e4f4f9b815410
1Jayso <adodey1@yahoo.com>
2020-09-10 03:05:28 +0000

    Added pagination

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.contrib.sessions.backends import file
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView, 
@@ -28,11 +29,11 @@ from .models import Post
 # ]
 
 
-def home(request):
-    context = {
-        'posts' : Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+# def home(request):
+#     context = {
+#         'posts' : Post.objects.all()
+#     }
+#     return render(request, 'blog/home.html', context)
 
 
 class PostListView(ListView):
@@ -40,9 +41,26 @@ class PostListView(ListView):
     template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     odering = ['-date_posted']
+    paginate_by = 5
+
+# Displays a particular user's posts
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    odering = ['-date_posted']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user =  get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
 
 class PostDetailView(DetailView):
     model = Post
+
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
